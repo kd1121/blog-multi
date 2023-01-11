512e922fee3f4a220780bbae53b21a295336fe5b
Kishan Maniya <kishanmaniya001@gmail.com>
2023-01-11 11:59:31 +0530

    blog-data-fetch

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { blogData } from '../Sevices/blogData';
 
 const Blog = () => {
+    const {id} = useParams();
+    const [blog, setBlog] = useState(null)
+
+    useEffect(() => {
+      let blog=blogData.find(blog => blog.id === parseInt(id));
+
+      if (blog) {
+        setBlog(blog)        
+      }
+    }, [])
+    
   return (
+    <>
     <div>
-      Blog Page
+        <Link to='/blogs'>Go Back</Link>
+        
+            {
+                blog ?
+                <div>
+                    <header>
+                        <h1>{blog.title}</h1>
+                    </header>
+                </div>: null
+            }
+              
     </div>
+    </>
   )
 }
 
