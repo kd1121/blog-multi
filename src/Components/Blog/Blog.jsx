db0e00c36a2a1c1ee818dbec2d239b40e9385be9
Kishan Maniya <kishanmaniya001@gmail.com>
2023-01-13 11:15:15 +0530

    call-from-id

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-// import { blogData } from '../Sevices/blogData';
 import axios from 'axios';
-// import Blogs from '../Sevices/Blogs';
-import "../Blog/Blog.css"
-// import Navbar from '../Home/Navbar';
 import { FaAngleLeft } from "react-icons/fa";
+import "../Blog/Blog.css"
+
+// import Blogs from '../Sevices/Blogs';
+// import { blogData } from '../Sevices/blogData';
+// import Navbar from '../Home/Navbar';
 
 const Blog = () => {
     
@@ -21,17 +22,59 @@ const Blog = () => {
       const getBlog = async () => {
         const response = await axios.get(apiEndpoint);
         setBlog(response.data);
-        console.log(response.data)
+        // console.log(response.data)
       }
       getBlog();
-      
-
-      
 
       // if (blog) {
       //   setBlog(blog)        
       // }
-    }, [])
+    }, [id])
+
+    //==============================================================================================================================
+
+    // useEffect(() => {
+    //   // let blog=blogData.find(blog => blog.id === parseInt(id));
+
+    //   const getBlog = async () => {
+    //     const response = await axios.get(`${apiEndpoint} + ${id}`);
+    //     setBlog(response.data);
+    //     console.log(response.data)
+    //   }
+    //   getBlog();
+
+      
+
+      
+
+    //   // if (blog) {
+    //   //   setBlog(blog)        
+    //   // }
+    // }, [id])
+
+
+    // return(
+    //   <>
+    //   <div className='blog-main'>
+    //     <Link className='go-back' to='/blogs'><FaAngleLeft size='25pt' /></Link>
+        
+    //         <div>
+    //           {/* <h1><span>BLOGER</span></h1> */}
+    //           <h1 className='blog-title'>{blogs.title}</h1>
+    //           {/* <h3>Description</h3> */}
+    //           <h3 className='blog-des'>{blogs.description}</h3>
+    //           <h6 className='sep-line'>By {blogs.writerName}</h6>
+    //           <h6 className='dt-line'>Created {blogs.createdDate.slice(0,10)}</h6>
+    //         </div>
+       
+              
+    // </div>
+    //   </>
+    // )
+    
+    //==============================================================================================================================
+
+
     
   return (
     <>
@@ -59,4 +102,4 @@ const Blog = () => {
   )
 }
 
-export default Blog
+export default Blog;
