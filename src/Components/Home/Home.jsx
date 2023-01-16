bf99120aed5bd19da8424efce282ed3da38fac63
Kishan Maniya <kishanmaniya001@gmail.com>
2023-01-16 10:43:25 +0530

    home-page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,24 @@
 
 import React from 'react'
 import Navbar from './Navbar'
+import "../Home/Home.css"
+// import { color } from '@mui/system'
 
 const Home = () => {
   return (
+    <>
     <div>
       <Navbar />
-      Home Page
+      <div className='home-main'>
+      <div className='home-head'>
+        Hey! 
+        <div className='spaning'>Welcome to <span id='comma'>"</span><span id='main-txt'>Bloger</span><span id='comma'>"</span></div>
+        
+      </div>
+      </div>
     </div>
+    </>
+    
   )
 }
 
